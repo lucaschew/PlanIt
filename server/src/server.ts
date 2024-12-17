0e3d136c710a1330bdc5c6ad4204569130320d44
lucaschew <lucaschew0@gmail.com>
2024-12-17 05:35:48 -0500

    remove slash

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: 'https://mcgill-planit.vercel.app/',
+    origin: 'https://mcgill-planit.vercel.app',
     methods: 'GET,POST,PATCH,DELETE',
     credentials: true, // Include cookies if necessary
   })
