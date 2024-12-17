f0b3bf4cf379602fa73829eb9a3645300af11dae
lucaschew <lucaschew0@gmail.com>
2024-12-17 01:18:03 -0500

    always send credentials

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import AppRoutes from './routes'
 import './styles/index.css'
+import axios from 'axios'
 
 export default function App() {
+  axios.defaults.withCredentials = true
   return <AppRoutes />
 }
